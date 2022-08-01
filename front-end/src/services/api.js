7fa76c5dfae4a24f22acde9108b71305a80cb3dc
Victor Matos <antoniovitomatos@gmail.com>
2022-08-01 12:17:24 -0300

    bug fix

--- front-end/src/services/api.js
+++ front-end/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost:5000"
+  baseURL: process.env.API_BASE_URL || "https://sing-me-a-song-driven.herokuapp.com"
 });
 
 export default instance;
